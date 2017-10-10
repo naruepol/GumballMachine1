31744446d77a4df0994b51db3aa63f9a94f9833b
naruepol <naruapon@gmail.com>
2017-10-10 15:18:07 +0700

    Initial commit

---
+++ src/GumballMachineTestDrive.java
@@ -0,0 +1,10 @@
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GumballMachine gumballMachine =  new GumballMachine(5);
+		
+	}
+
+}
