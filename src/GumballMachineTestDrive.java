3db160d182bd4307814700f8edf4d44a847bd6ec
naruepol <naruapon@gmail.com>
2017-10-10 16:25:12 +0700

    Updated GumballMachineTestDrive

--- src/GumballMachineTestDrive.java
+++ src/GumballMachineTestDrive.java
@@ -6,7 +6,35 @@ public class GumballMachineTestDrive {
 		GumballMachine gumballMachine =  new GumballMachine(5);
 		
 		System.out.println(gumballMachine);
-		//System.out.println(gumballMachine.toString());		
+		//System.out.println(gumballMachine.toString());	
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.ejectQuarter();		
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.ejectQuarter();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
 	}
 
 }
